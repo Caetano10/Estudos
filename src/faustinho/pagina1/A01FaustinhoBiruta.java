e246088b7dec24e41aa38bac2f57ae00adab2cc6
Caetano <caetanol420@gmail.com>
2024-02-05 23:21:54 -0300

    test1

--- src/faustinho/pagina1/A01FaustinhoBiruta.java
+++ src/faustinho/pagina1/A01FaustinhoBiruta.java
@@ -2,6 +2,6 @@ package faustinho.pagina1;
 
 public class A01FaustinhoBiruta {
     public static void main (String[] args){
-        System.out.println("Loucura Loucura Loucur");
+        System.out.println("Loucura Loucura Loucura");
     }
 }
